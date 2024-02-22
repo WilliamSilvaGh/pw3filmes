9ea30155266fbe6cc36aa9b3a5ba7ddde481a553
WilliamSilvaGh <williamsilvaesquilino@gmail.com>
2024-02-21 21:00:25 -0300

    Primeiro Commit

---
+++ PWFilmes/src/PWFilmes.Domain/Entities/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using PWFilmes.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWFilmes.Domain.Entities
+{
+    public class Categoria : EntityBase
+    {
+        public string Descricao { get; set; }
+
+        public Categoria(string descricao)
+        {
+            Id = Guid.NewGuid();
+            Descricao = descricao;
+        }
+    }
+}
---
+++ PWFilmes/src/PWFilmes.Domain/Entities/Filme.cs
@@ -0,0 +1,20 @@
+﻿using PWFilmes.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWFilmes.Domain.Entities
+{
+    public class Filme : EntityBase
+    {
+       public string Nome { get; set; }
+       public string Diretor { get; set; }
+       public string Atores { get; set; }
+       public short AnoLancamento { get; set; }
+       public virtual Categoria Categoria { get; set; }
+
+
+    }
+}
